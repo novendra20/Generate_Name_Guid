1cb0d9da6f472f7fbe5c9c27106fd226aacdc285
Novendra - Protelindo - CODE.ID <novendra@ptcode.id>
2025-11-12 09:44:27 +0700

    Update change with inputan

--- Generate_Name_Guid/Program.cs
+++ Generate_Name_Guid/Program.cs
@@ -4,24 +4,41 @@ using Generate_Name_Guid.DTO;
 
 var pathOutput = "C:\\Vendra\\OutputFIle\\File";
 var jsonOutput = "C:\\Vendra\\OutputFIle\\JSON";
+int count = 0;
+List<string> pathFile = new List<string>();
+string prefixFolder = string.Empty;
 
-args = ["12", "PMT", "C:\\Vendra\\Support\\ASOKA GRAHA SURYA (Batch 1).pdf"];
-
-if (args.Length != 3)
+Console.WriteLine("Masukkan Jumlah Total Generate:");
+if(!int.TryParse(Console.ReadLine(), out count))
 {
-    Console.WriteLine("Usage: Generate_Name_Guid <Count> <PrefixFolder> <SourceFile>");
-    Console.WriteLine("@Example: dotnet run -- 2 PMT \"D:\\File\\Source.pdf\" ");
-    return;
+    count = 0;
 }
 
-if (!int.TryParse(args[0], out var count) || count <= 0)
+if(count == 0)
 {
     Console.Error.WriteLine("Error: Count must be a positive integer.");
     return;
 }
 
-var prefixFolder = args[1];
-var pathFile = args.Skip(2).ToList();
+Console.WriteLine("Masukkan Prefix Folder:");
+prefixFolder = Console.ReadLine() ?? string.Empty;
+
+if(string.IsNullOrWhiteSpace(prefixFolder))
+{
+    Console.Error.WriteLine("Error: Prefix Folder cannot be empty.");
+    return;
+}
+
+Console.WriteLine("Masukkan PathFile, lebih dari satu pisah dengan koma: Ex: \"C:\\Vendra\\Support\\ASOKA GRAHA SURYA (Batch 1).pdf\"");
+pathOutput = Console.ReadLine() ?? string.Empty;
+
+if (string.IsNullOrWhiteSpace(pathOutput))
+{
+    Console.Error.WriteLine("Error: PathFile cannot be empty.");
+    return;
+}
+
+pathFile = pathOutput.Split(",").ToList();
 
 var respons = CheckingFileIsExist.FileIsExist(pathFile);
 if (!respons.IsExist)
